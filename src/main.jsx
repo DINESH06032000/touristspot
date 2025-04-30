3d7def77f90640a21e8d7fa5c366723bdfcb3fd0
DINESH06032000 <pd06032000@gmail.com>
2025-04-30 15:46:46 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Globe from './components/Globe.jsx'
+
 
 createRoot(document.getElementById('root')).render(
 
     <StrictMode>
       <App />
+  
+      {/* <Globe/> */}
     </StrictMode>
  
   ,
